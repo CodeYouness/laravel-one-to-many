5b7082ea1ef5df158e3c5ae05b8a5fa570cd0726
CodeYouness <ylijassi@gmail.com>
2024-08-01 16:29:36 +0200

    added one to many

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,16 +2,39 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $categoriesName = [
+            "Web Development",
+            "Application Security",
+            "News",
+            "IT",
+            "Networks",
+            "Operating Systems",
+            "Gaming",
+            "Devops",
+            "Database",
+            "front-end",
+            "Back-end",
+            "Full-stack",
+
+        ];
+
+        foreach ($categoriesName as $categoryName) {
+            $category = new Category();
+            $category->name = $categoryName;
+            $category->color = $faker->unique()->safeHexColor();
+            $category->save();
+        }
     }
 }
